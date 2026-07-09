0aef858033744d651f868ca1fc0a09d1576dcf77
Arch - Muhammad Abduh <imuhammadabduh@gmail.com>
2026-07-09 22:55:25 +0700

    fix: google OAuth - pass clientSecret and redirect_uri to OAuth2Client

--- app/src/components/MainLayout.tsx
+++ app/src/components/MainLayout.tsx
@@ -7,12 +7,12 @@ import Navbar from "@/components/Navbar";
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const isLanding = pathname === "/";
+  const isLogin = pathname === "/login";
 
   return (
     <div className="flex h-dvh flex-col">
-      {!isLanding && <Navbar />}
-      
-       <main className={`flex-1 w-full pb-14 md:pb-0 overflow-y-auto ${!isLanding ? 'pt-16' : ''}`}>
+      {!isLanding && !isLogin && <Navbar />}
+      <main className={`flex-1 w-full overflow-y-auto ${!isLanding && !isLogin ? 'pt-16' : ''} ${!isLogin ? 'pb-14 md:pb-0' : ''}`}>
         {children}
       </main>
     </div>
